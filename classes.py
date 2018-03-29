e794dd0f7c100be9b00b5408ced0bda1120d5875
dev271 <devgupt27@gmail.com>
2018-03-29 18:31:10 +0530

    Update classes.py

--- classes.py
+++ classes.py
@@ -5,90 +5,106 @@ import math
 
 class Maze():
 
-    def __init__(self):
-	self.BLACK = (0,0,0)
-	self.GOLD = (246,253,49)
-	self.GREY = (50,50,50)
-	self.RED = (255,0,0)
-	self.BLUE = (20,27,229)
-	self.WHITE = (255,255,255)	
-	self.GREEN = (0,255,0)
-	self.CYAN = (0,255,255)	
-	self.width  = 20
-	self.height = 20
-	self.margin = 1	
-	self.score = 0
-	self.level = 1	
-	self.grid = []
-	self.wall = []
-	self.WALL = [[0 for i in range(1000)] for j in range(1000)]
-	self.intersections = [(1,5),(4,3),(4,7),(3,5),(6,3),(6,5),(6,7),(13,3),(13,5),(13,7),(15,3),(15,7),(16,5),(18,5)]
-	self.countfinal = 0
-	self.make()
-	self.size = [1200, 700]
-	self.screen = pygame.display.set_mode(self.size)
+	def __init__(self):
+		self.BLACK = (0,0,0)
+		self.GOLD = (246,253,49)
+		self.GREY = (50,50,50)
+		self.RED = (255,0,0)
+		self.BLUE = (20,27,229)
+		self.WHITE = (255,255,255)	
+		self.GREEN = (0,255,0)
+		self.CYAN = (0,255,255)	
+		self.width  = 20
+		self.height = 20
+		self.margin = 1	
+		self.score = 0
+		self.level = 1	
+		self.num_wall = 0
+		self.grid = []
+		self.wall = []
+		self.WALL = [[0 for i in range(1000)] for j in range(1000)]
+		self.intersections = [(1,5),(4,3),(4,7),(3,5),(6,3),(6,5),(6,7),(13,3),(13,5),(13,7),(15,3),(15,7),(16,5),(18,5)]
+		self.countfinal = 0
+		self.make()
+		self.size = [1200, 700]
+		self.screen = pygame.display.set_mode(self.size)
 
-    def make(self):
+	def make(self):
 		for i in range(0,20):
 			self.wall.append([i,0])
 			self.wall.append([i,10])
 			self.WALL[i][0] = 1
 			self.WALL[i][10] = 1
+			self.num_wall += 2
 		for j in range(0,11):
 			self.wall.append([0,j])
 			self.wall.append([19,j])
 			self.WALL[0][j] = 1
 			self.WALL[19][j] = 1
+			self.num_wall += 2
 		for j in range(1,3):
 			self.wall.append([5,j])
 			self.wall.append([14,j])
 			self.WALL[5][j] = 1
-			self.WALL[14][j] = 1	
+			self.WALL[14][j] = 1
+			self.num_wall += 2	
 		for i in range(2,4):
 			self.wall.append([i,2])
 			self.WALL[i][2] = 1
+			self.num_wall += 1
 		for i in range(7,13):
 			self.wall.append([i,2])
 			self.WALL[i][2] = 1
+			self.num_wall += 1
 		for i in range(16,18):
 			self.wall.append([i,2])
 			self.WALL[i][2] = 1
+			self.num_wall += 1
 		for j in range(3,5):
 			self.wall.append([2,j])
 			self.wall.append([17,j])
 			self.WALL[2][j] = 1
 			self.WALL[17][j] = 1
+			self.num_wall += 2
 		for i in range(4,6):
 			self.wall.append([i,4])
 			self.wall.append([i,6])
 			self.WALL[i][4] = 1
 			self.WALL[i][6] = 1
+			self.num_wall += 2
 		for i in range(14,16):
 			self.wall.append([i,4])
 			self.wall.append([i,6])
 			self.WALL[i][4] = 1
 			self.WALL[i][6] = 1
+			self.num_wall += 2
 		for i in range(7,9):
 			self.wall.append([i,4])
 			self.WALL[i][4] = 1
+			self.num_wall += 1
 		for i in range(11,13):
 			self.wall.append([i,4])
 			self.WALL[i][4] = 1
+			self.num_wall += 1
 		for i in range(7,13):
 			self.wall.append([i,6])
 			self.WALL[i][6] = 1
+			self.num_wall += 1
 		self.wall.append([7,5])
 		self.WALL[7][5] = 1
 		self.wall.append([12,5])
 		self.WALL[12][5] = 1
+		self.num_wall += 2
 		for j in range(6,8):
 			self.wall.append([2,j])
 			self.wall.append([17,j])
 			self.WALL[2][j] = 1
 			self.WALL[17][j] = 1
+			self.num_wall += 2
 		for i in range(7,13):
 			self.wall.append([i,8])
 			self.WALL[i][8] = 1
+			self.num_wall += 1
 		self.wall.append([2,8])
 		self.WALL[2][8] = 1
 		self.wall.append([3,8])
@@ -97,11 +113,13 @@ class Maze():
 		self.WALL[16][8] = 1
 		self.wall.append([17,8])
 		self.WALL[17][8] = 1
+		self.num_wall += 4
 		for j in range(8,10):
 			self.wall.append([5,j])
 			self.wall.append([14,j])
 			self.WALL[5][j] = 1
 			self.WALL[14][j] = 1
+			self.num_wall += 2
 		for row in range(11):
 			self.grid.append([])
 			for column in range(20):
@@ -110,7 +128,7 @@ class Maze():
 			self.grid[wall[1]][wall[0]]=1 	
 		return self
 
-    def reset(self):
+	def reset(self):
 		self.grid = []
 		self.wall = []
 		self.WALL = [[0 for i in range(1000)] for j in range(1000)]
@@ -118,15 +136,15 @@ class Maze():
 		self.make()	
 		return self
 
-    def scoredisp(self):
-        scoretext=self.scorefont.render("Score: "+(str)(self.score), 1,self.WHITE)
-        self.screen.blit(scoretext, (30, 650))
+	def scoredisp(self):
+	        scoretext=self.scorefont.render("Score: "+(str)(self.score), 1,self.WHITE)
+	        self.screen.blit(scoretext, (30, 650))
 
-    def leveldisp(self):
-        leveltext=self.scorefont.render("Level: "+(str)(self.level), 1,self.WHITE)
-        self.screen.blit(leveltext, (600, 650))
+	def leveldisp(self):
+	        leveltext=self.scorefont.render("Level: "+(str)(self.level), 1,self.WHITE)
+	        self.screen.blit(leveltext, (600, 650))
 
-    def dispmaze(self):
+	def dispmaze(self):
 		for row in range(11):
 			for column in range(20):
 				color = self.GREY
@@ -137,29 +155,29 @@ class Maze():
 				else:
 					pygame.draw.rect(self.screen,color,[(self.margin+self.width)*column+self.margin+7,(self.margin+self.height)*row+self.margin+7,self.width-14,self.height-14])
 
-    def drawwall(self):
+	def drawwall(self):
 		for wall in self.wall:
 			pygame.draw.rect(self.screen,self.BLUE,[(self.margin+self.width)*wall[0]+self.margin,(self.margin+self.height)*wall[1]+self.margin,self.width,self.height])
 
 class Person(Maze):
 
 	def __init__(self,x,y):
-	    self.x = x	
-	    self.y = y
+		self.x = x	
+		self.y = y
 	
 	def checkWall(self,x,y,W):
-	    if [x,y] in W:
+		if [x,y] in W:
 			return True
-	    else:
+		else:
 			return False
 
 	def moveleft(self,W):
-	    if self.x > 0:
-		if self.checkWall(self.x-1,self.y,W):                
-			self.x=self.x
-		else:
-			self.x=(self.x)-1
-	    return self		
+		if self.x > 0:
+			if self.checkWall(self.x-1,self.y,W):                
+				self.x=self.x
+			else:
+				self.x=(self.x)-1
+		return self		
 
 	def moveright(self,W):
 		if self.x < 19:
@@ -200,46 +218,46 @@ class Pacman(Person):
 		Person.__init__(self,x,y)
 	
 	def collectCoin(self,Wa):
-	    if Wa.grid[self.y][self.x] == 0:
+		if Wa.grid[self.y][self.x] == 0:
 			return True
-	    else:
+		else:
 			return False
 
 	def pacleft(self,Wa):
-	    Person.moveleft(self,Wa.wall)
-	    if self.collectCoin(Wa):	    
+		Person.moveleft(self,Wa.wall)
+		if self.collectCoin(Wa):	    
 			Wa.grid[self.y][self.x]=1
 			Wa.score += 1
-	    return self
+		return self
 
 	def pacright(self,Wa):
-	    Person.moveright(self,Wa.wall)
-	    if self.collectCoin(Wa):	    
+		Person.moveright(self,Wa.wall)
+		if self.collectCoin(Wa):	    
 			Wa.grid[self.y][self.x]=1
 			Wa.score += 1
-	    return self
+		return self
 
 	def pacup(self,Wa):
-	    Person.moveup(self,Wa.wall)
-	    if self.collectCoin(Wa):	    
+		Person.moveup(self,Wa.wall)
+		if self.collectCoin(Wa):	    
 			Wa.grid[self.y][self.x]=1
 			Wa.score += 1
-	    return self
+		return self
 
 	def pacdown(self,Wa):
-	    Person.movedown(self,Wa.wall)
-	    if self.collectCoin(Wa):	    
+		Person.movedown(self,Wa.wall)
+		if self.collectCoin(Wa):	    
 			Wa.grid[self.y][self.x]=1
 			Wa.score += 1
-	    return self
+		return self
 
 	def draw(self,G):
-	    pygame.draw.rect(G.screen,G.GREEN,[(G.margin+G.width)*self.x+G.margin,(G.margin+G.height)*self.y+G.margin,G.width,G.height])
+		pygame.draw.rect(G.screen,G.GREEN,[(G.margin+G.width)*self.x+G.margin,(G.margin+G.height)*self.y+G.margin,G.width,G.height])
 
 	def checkGhost(self,V):
-	    if [self.x,self.y] == [V.x,V.y]:
+		if [self.x,self.y] == [V.x,V.y]:
 			return True
-	    else:
+		else:
 			return False
 
 	def pacmove(self,G,action):
@@ -258,12 +276,12 @@ class Pacman(Person):
 		# 	self.dirc = 3
 		if action is 0:
 			self.pacright(G)
-			self.dirc = 0
-		elif action is 1:
-			self.pacup(G)
 			self.dirc = 1
-		elif action is 2:
+		elif action is 1:
 			self.pacleft(G)
+			self.dirc = 0
+		elif action is 2:
+			self.pacup(G)
 			self.dirc = 2
 		elif action is 3:
 			self.pacdown(G)
@@ -285,28 +303,28 @@ class Blinky(Person,Pacman):
 		return self
 
 	def bleft(self,Wa):
-	    Person.moveleft(self,Wa.wall)
-	    return self
+		Person.moveleft(self,Wa.wall)
+		return self
 
 	def bright(self,Wa):
-	    Person.moveright(self,Wa.wall)
-	    return self
+		Person.moveright(self,Wa.wall)
+		return self
 
 	def bup(self,Wa):
-	    Person.moveup(self,Wa.wall)
-	    return self
+		Person.moveup(self,Wa.wall)
+		return self
 
 	def bdown(self,Wa):
-	    Person.movedown(self,Wa.wall)
-	    return self
+		Person.movedown(self,Wa.wall)
+		return self
 
 	def draw(self,G):
-	    pygame.draw.rect(G.screen,G.RED,[(G.margin+G.width)*self.x+G.margin,(G.margin+G.height)*self.y+G.margin,G.width,G.height])
+		pygame.draw.rect(G.screen,G.RED,[(G.margin+G.width)*self.x+G.margin,(G.margin+G.height)*self.y+G.margin,G.width,G.height])
  
 	def blinkymove(self,G,pac):
 		if (self.x,self.y) not in G.intersections:
 			if ([self.x-1,self.y] not in G.wall and self.prev != 1) :
-					move = 0
+				move = 0
 			elif ([self.x+1,self.y] not in G.wall and self.prev != 0):
 				move = 1
 			elif ([self.x,self.y-1] not in G.wall and self.prev != 3):
@@ -353,28 +371,28 @@ class Inky(Person,Pacman):
 		return self
 
 	def ileft(self,Wa):
-	    Person.moveleft(self,Wa.wall)
-	    return self
+		Person.moveleft(self,Wa.wall)
+		return self
 
 	def iright(self,Wa):
-	    Person.moveright(self,Wa.wall)
-	    return self
+		Person.moveright(self,Wa.wall)
+		return self
 
 	def iup(self,Wa):
-	    Person.moveup(self,Wa.wall)
-	    return self
+		Person.moveup(self,Wa.wall)
+		return self
 
 	def idown(self,Wa):
-	    Person.movedown(self,Wa.wall)
-	    return self
+		Person.movedown(self,Wa.wall)
+		return self
 
 	def draw(self,G):
-	    pygame.draw.rect(G.screen,G.CYAN,[(G.margin+G.width)*self.x+G.margin,(G.margin+G.height)*self.y+G.margin,G.width,G.height])
+		pygame.draw.rect(G.screen,G.CYAN,[(G.margin+G.width)*self.x+G.margin,(G.margin+G.height)*self.y+G.margin,G.width,G.height])
  
 	def inkymove(self,G,pac):
 		if (self.x,self.y) not in G.intersections:
 			if ([self.x-1,self.y] not in G.wall and self.prev != 1) :
-					move = 0
+				move = 0
 			elif ([self.x+1,self.y] not in G.wall and self.prev != 0):
 				move = 1
 			elif ([self.x,self.y-1] not in G.wall and self.prev != 3):
@@ -387,17 +405,17 @@ class Inky(Person,Pacman):
 			tx = 0
 			ty = 0
 			if pac.dirc is 2:
-    				ty	 = pac.y
+    				ty = pac.y
 				tx = max(0,pac.x-2)
 			elif pac.dirc is 0:
-					ty = pac.y
-					tx = min(19,pac.x+2)
+				ty = pac.y
+				tx = min(19,pac.x+2)
 			elif pac.dirc is 1:
-					tx = pac.x
-					ty = max(0,pac.y - 2)
+				tx = pac.x
+				ty = max(0,pac.y - 2)
 			elif pac.dirc is 3:
-					tx = pac.x
-					ty = min(10,pac.y+2)
+				tx = pac.x
+				ty = min(10,pac.y+2)
 			if [self.x-1,self.y] not in G.wall:
 				dist[0] = math.sqrt((tx-self.x+1)*(tx-self.x+1)+(ty-self.y)*(ty-self.y))
 			if [self.x+1,self.y] not in G.wall:
